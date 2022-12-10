29968d5cead2ba32d20f8e94aec3893c579fd044
Boukraa Mohamed <1745816+bm2ilabs@users.noreply.github.com>
2022-12-10 19:02:06 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,8 @@ echo "[default]
 aws_access_key_id = ${S3_ACCESS_KEY_ID}
 aws_secret_access_key = ${S3_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
+echo S3_BUCKET ${S3_BUCKET}
+
 aws s3 cp ${FILE} s3://${S3_BUCKET}/ --region=${S3_REGION} --endpoint-url=${S3_ENDPOINT_URL} --cli-connect-timeout 6000 $*
 
 rm -rf ~/.aws
